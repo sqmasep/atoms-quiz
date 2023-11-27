abff992850a6c3fb9905009efc573c37c6d88a25
sqmasep <leblond.romain@orange.fr>
2023-11-27 22:06:04 +0100

    [FEAT] Added PropsOverrides complex type for cool syntax (and updated code snippets as a consequence)

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -11,3 +11,38 @@ export type Except<
   TObj extends Record<PropertyKey, any>,
   TKey extends LiteralUnion<keyof TObj>,
 > = Omit<TObj, TKey>;
+
+export interface WithChildren {
+  children: React.ReactNode;
+}
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type PropsOverrides<
+  TElement extends React.ElementType<any>,
+  TExclude extends
+    | LiteralUnion<keyof React.ComponentPropsWithoutRef<TElement>>
+    | Record<PropertyKey, any> = never,
+> = Omit<
+  React.ComponentPropsWithoutRef<TElement>,
+  TExclude extends string
+    ? TExclude
+    : TExclude extends Record<PropertyKey, any>
+      ? keyof TExclude
+      : never
+> &
+  TExclude extends string
+  ? Omit<
+      React.ComponentPropsWithoutRef<TElement>,
+      TExclude extends string ? TExclude : never
+    >
+  : TExclude extends Record<PropertyKey, any>
+    ? TExclude
+    : never;
+
+// &
+//   (TExclude extends string
+//     ? { B: string }
+//     : TExclude extends Record<PropertyKey, any>
+//       ? TExclude
+//       : { A: string });
+/* eslint-enable @typescript-eslint/no-explicit-any */
