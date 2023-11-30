6115e82cd416d573db8986f4def341277a8bced0
sqmasep <leblond.romain@orange.fr>
2023-11-30 22:18:13 +0100

    [FEAT] AnswerContainer, added input from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,46 @@
+import { parse } from "valibot";
+import Atom from "~/components/Atom";
+import List from "~/components/List";
 import { Button } from "~/components/ui/button";
+import AnswerContainer from "~/features/answer/components/AnswerContainer";
+import Navbar from "~/layouts/Navbar";
+import { atomsSchema } from "~/lib/validation/atomSchema";
 
 export default async function Home() {
-  const res = (await fetch(
+  const res = await fetch(
     process.env.ATOMS_API_URL ?? "http://localhost:3000/api/atoms",
-  ).then(async res => res.json())) as {};
+  ).then(async res => res.json());
 
-  console.log(res);
+  // to perform validation, i'll go for valibot, since it's smaller and i don't need
+  // to validate the whole thing, just a few fields
+  // on top of that it'll make me learn valibot because i know zod already
+  const atoms = parse(atomsSchema, res);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
+      <Navbar />
       <div className="pointer-events-none fixed inset-0 -z-10 bg-[url('/noise.gif')] bg-[length:300px] opacity-5" />
       <div className="pointer-events-none fixed top-10 -z-10 h-1/3 w-1/2 bg-slate-600 blur-[400px]" />
 
-      <pre>{JSON.stringify(res, null, 2)}</pre>
-      <div className="flex w-36 flex-col rounded-lg border-4 border-solid border-slate-300 p-4 shadow ">
-        <span className="text-slate-500">3</span>
-        <span className="text-2xl">Li</span>
-        <span className="text-slate-500">Lithium</span>
-      </div>
+      <AnswerContainer />
 
-      <Button variant="secondary">testok</Button>
+      {/* <pre>{JSON.stringify(res, null, 2)}</pre> */}
+
+      {/* <div className="grid grid-cols-12">
+        <List of={atoms}>
+          {atom => (
+            <Atom
+              key={atom.atomicNumber}
+              atomicNumber={atom.atomicNumber}
+              symbol={atom.symbol}
+              name={atom.name.fr}
+              color="#000"
+              // FIXME [LAYOUT] w-[125px] is not a good solution and i need to fix the flex container
+              className="aspect-square w-[125px]"
+            />
+          )}
+        </List>
+      </div> */}
     </main>
   );
 }
---
+++ src/features/answer/components/AnswerContainer.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import useModes from "~/hooks/useModes";
+import WriteAnswerInput from "./WriteAnswerInput";
+import ChoiceCards from "./ChoiceCards";
+
+interface AnswerContainerProps {}
+
+const AnswerContainer: React.FC<
+  AnswerContainerProps &
+    Omit<React.ComponentPropsWithoutRef<"div">, keyof AnswerContainerProps>
+> = ({ ...props }) => {
+  const { answerType } = useModes();
+  return (
+    <div {...props} className="mx-auto mt-48 max-w-3xl">
+      {answerType === "write-answer" && <WriteAnswerInput />}
+      {answerType === "options" && <ChoiceCards />}
+    </div>
+  );
+};
+
+export default AnswerContainer;
