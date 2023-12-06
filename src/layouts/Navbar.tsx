4adc2cb0e3d5239d652fc8c1416b2495775a553e
sqmasep <leblond.romain@orange.fr>
2023-12-06 21:21:34 +0100

    [FEAT] Navbar & types

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,7 +1,42 @@
-const Navbar = () => {
+"use client";
+
+import { useEffect } from "react";
+import { Progress } from "~/components/ui/progress";
+import AnswerOptionsSelect from "~/features/modes/components/AnswerOptionsSelect";
+import CollectionOptionsSelect from "~/features/modes/components/CollectionOptionsSelect";
+import GuessOptionsSelect from "~/features/modes/components/GuessOptionsSelect";
+import SortingOptionsSelect from "~/features/modes/components/SortingOptionsSelect";
+import SettingsDropdown from "~/features/settings/components/SettingsDropdown";
+import useModes from "~/hooks/useModes";
+import { useProgression } from "~/stores/progression";
+
+const Navbar: React.FC = () => {
+  // TODO use `useModes` or something for the selected options
+  const { answerType, collection, guess, sort } = useModes();
+  const progression = useProgression();
+
+  useEffect(() => {
+    progression.reset();
+  }, [answerType, collection, guess, sort]);
+
   return (
-    <nav>
-      <h1>Navbar</h1>
-    </nav>
+    <>
+      <Progress
+        value={(progression.correctAnswers * 100) / progression.atoms.length}
+      />
+
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-1">
+          <AnswerOptionsSelect />
+          <SortingOptionsSelect />
+          <GuessOptionsSelect />
+          <CollectionOptionsSelect />
+        </div>
+
+        <SettingsDropdown />
+      </div>
+    </>
   );
 };
+
+export default Navbar;
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,11 +2,10 @@ import type { Variants } from "framer-motion";
 import type { LiteralUnion as TypeFestLiteralUnion } from "type-fest";
 
 export type NonEmptyTuple<TElement> = readonly [TElement, ...TElement[]];
-
 export type LiteralUnion<TString> = TypeFestLiteralUnion<TString, string>;
+export type Nullish<T> = T | null | undefined;
 
 export type { Except as StrictExcept } from "type-fest";
-
 export type Except<
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
   TObj extends Record<PropertyKey, any>,
@@ -18,3 +17,53 @@ export interface WithChildren {
 }
 
 export type AnimationRelation = Record<"parent" | "child", Variants>;
+
+// type F = Extract<{ epic: string; jar: number } | "Hello", string>;
+// type G = Extract<{ epic: string; jar: number } | "Hello", AnyRecord>;
+// //   ^?
+
+// type AnyRecord = Record<PropertyKey, any>;
+
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// export type PropsOverrides<
+//   TElement extends React.ElementType<any>,
+//   TExcludeProps extends
+//     | LiteralUnion<keyof TElementProps>
+//     | AnyRecord
+//     | undefined = undefined,
+//   TElementProps extends AnyRecord = React.ComponentPropsWithoutRef<TElement>,
+//   TObjProps = TExcludeProps extends undefined
+//     ? TElementProps
+//     : TExcludeProps extends AnyRecord
+//       ? Extract<TExcludeProps, AnyRecord>
+//       : TElementProps,
+//   TStringProps extends PropertyKey = TExcludeProps extends undefined
+//     ? keyof TElementProps
+//     : Extract<TExcludeProps, string>,
+// > = Omit<TElementProps, TStringProps | keyof TObjProps> &
+//   (TExcludeProps extends undefined
+//     ? // if no properties to omit are declared, return all properties
+//       TElementProps
+//     : // if at least one property is declared,
+//       TExcludeProps extends AnyRecord
+//       ? // overload for object properties
+//         TExcludeProps
+//       : TStringProps extends keyof TElementProps
+//         ? Extract<TElementProps, TStringProps>
+//         : unknown);
+
+// type Q = Omit<{ epic: string; jar: number }, "epic"> & unknown;
+// //   ^?
+
+// /* eslint-enable @typescript-eslint/no-explicit-any */
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+
+// type Z = PropsOverrides<"a">;
+// //   ^?
+// const z: Z = {};
+
+// const a: PropsOverrides<"a", "href"> = {};
+// const b: PropsOverrides<"a", { jar: string }> = {};
+// const c: PropsOverrides<"a", "href" | { jar: string }> = {};
+// const d: PropsOverrides<"a", "href" | { href: number }> = { href: "" };
+// const e: PropsOverrides<"a"> = {};
