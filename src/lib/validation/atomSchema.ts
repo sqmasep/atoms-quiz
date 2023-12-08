0f1250dfa414d3028b86b2080e485f8e9736ab5f
sqmasep <leblond.romain@orange.fr>
2023-12-08 17:30:46 +0100

    [FIX] Updated atom schema to adapt my api

--- src/lib/validation/atomSchema.ts
+++ src/lib/validation/atomSchema.ts
@@ -14,37 +14,31 @@ import {
 export const atomSchema = object({
   atomicNumber: number([minValue(1)]),
   symbol: string(),
-  // atomicMass: string(),
-  // meltingPoint: object({
-  //   kelvin: string(),
-  //   celsius: string(),
-  //   fahrenheit: string(),
-  // }),
+  atomicMass: string(),
+  meltingPoint: object({
+    kelvin: string(),
+    celsius: string(),
+    fahrenheit: string(),
+  }),
   name: object({
     en: string(),
     fr: string(),
   }),
   phaseAtSTP: picklist(["gas", "liquid", "solid", "plasma"]),
-  block: object({
-    full: string(),
-    shorten: string(),
-  }),
+  block: string(),
   discovery: object({
     by: string(),
     country: string(),
     year: number([integer()]),
   }),
-  // family: object({
-  //   isMetal: boolean(),
-  //   name: string(),
-  // }),
-  // // WARN not sure about that
-  // group: number([minValue(1), maxValue(18)]),
-  // period: number([minValue(1), maxValue(7)]),
+  family: object({
+    isMetal: boolean(),
+    name: string(),
+  }),
+  group: number([minValue(1), maxValue(18)]),
+  period: number([minValue(1), maxValue(7)]),
 });
-
 export type AtomType = Output<typeof atomSchema>;
 
 export const atomsSchema = array(atomSchema);
-
 export type AtomsType = Output<typeof atomsSchema>;
