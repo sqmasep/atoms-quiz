507d45737f494bef67c9fd163bd67beb5f80319c
sqmasep <leblond.romain@orange.fr>
2023-12-09 05:27:38 +0100

    [FEAT] Added restart & other actions & fixed massive bug on `atoms.sort` usage

--- src/components/GameHandler.tsx
+++ src/components/GameHandler.tsx
@@ -7,6 +7,14 @@ import useModes from "~/hooks/useModes";
 import Navbar from "~/layouts/Navbar";
 import type { AtomsType } from "~/lib/validation/atomSchema";
 import { useProgression } from "~/stores/progression";
+import { Button } from "./ui/button";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
 interface GameHandlerProps {
   atoms: AtomsType;
@@ -18,32 +26,73 @@ const GameHandler: React.FC<
 > = ({ atoms, ...props }) => {
   const { answerType, collection, guess, sort } = useModes();
 
-  // TODO use `sort` mode
-  // const sortedAtoms = atoms;
-
   const progression = useProgression();
 
   useEffect(() => {
-    progression.setAtoms(atoms);
-  }, [atoms, progression]);
+    progression.reset();
+    const sortedAtoms = atoms.toSorted((a, b) => {
+      if (sort === "random") return Math.random() - 0.5;
+      if (sort === "atomic-number") return a.atomicNumber - b.atomicNumber;
+      return 0;
+    });
+
+    console.log(sortedAtoms);
+
+    progression.setAtoms(sortedAtoms);
+    progression.setCurrentAtom(sortedAtoms[0]);
+  }, [sort, guess]);
 
   // function nextQuestion() {
   //   const nextAtom = sortedAtoms[sortedAtoms.indexOf(currentAtom) + 1];
   //   setCurrentAtom(nextAtom);
   // }
 
-  // TODO probably should update `progression.atoms` when modes change and reset everything
   // useEffect(() => {}, [answerType, collection, guess, sort]);
+
+  function handlePlay() {
+    progression.reset();
+    const sortedAtoms = atoms.toSorted((a, b) => {
+      if (sort === "random") return Math.random() - 0.5;
+      if (sort === "atomic-number") return a.atomicNumber - b.atomicNumber;
+      return 0;
+    });
+
+    progression.setAtoms(sortedAtoms);
+    progression.setCurrentAtom(sortedAtoms[0]);
+  }
 
   return (
     <div {...props}>
       <Navbar />
-      <AtomsCounter
-        currentCount={progression.correctAnswers}
-        outOf={progression.atoms.length}
-      />
+      <div className="flex justify-center">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                onClick={() => progression.reset()}
+                size="icon"
+                variant="ghost"
+                className="text-neutral-500"
+              >
+                <ReloadIcon />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>Restart</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
 
+        <AtomsCounter
+          currentCount={progression.correctAnswers + progression.skippedAnswers}
+          outOf={progression.atoms.length}
+          correctCount={progression.correctAnswers}
+          incorrectCount={progression.incorrectAnswers}
+          skippedCount={progression.skippedAnswers}
+        />
+
+        <Button onClick={() => progression.skip()}>Skip</Button>
+      </div>
       <AnswerContainer />
+      <Button onClick={handlePlay}>play!</Button>
     </div>
   );
 };
