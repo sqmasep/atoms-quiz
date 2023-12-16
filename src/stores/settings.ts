0e90ea7c555468bd06da21c444ad7ebaf7a7e75e
sqmasep <leblond.romain@orange.fr>
2023-12-16 20:30:33 +0100

    [FIX] useSettings: Removed stupid workaround and added a subscriber for data persistence

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,16 +1,69 @@
-import { proxy, useSnapshot } from "valtio";
+import {
+  array,
+  boolean,
+  fallback,
+  getDefaults,
+  object,
+  picklist,
+  safeParse,
+} from "valibot";
+import { proxy, subscribe, useSnapshot } from "valtio";
 import {
   GUESS_OPTIONS,
   type GuessOptionValue,
 } from "~/features/modes/data/guessOptions";
 
-export const settingsStore = proxy({
+const defaultSettings = {
   shouldShowTimer: true,
   shouldSkipAnimations: false,
   hasSound: true,
   shouldShowMinimap: true,
   shouldAutoSend: false,
   guessLanguage: "en" as "en" | "fr",
+
+  atomView: GUESS_OPTIONS.map(a => a.value),
+
+  hasChangedSettingsDuringGame: false,
+} as const;
+
+const localStorageSettingsSchema = object({
+  shouldShowTimer: fallback(boolean(), defaultSettings.shouldShowTimer),
+  shouldSkipAnimations: fallback(
+    boolean(),
+    defaultSettings.shouldSkipAnimations,
+  ),
+  hasSound: fallback(boolean(), defaultSettings.hasSound),
+  shouldShowMinimap: fallback(boolean(), defaultSettings.shouldShowMinimap),
+  shouldAutoSend: fallback(boolean(), defaultSettings.shouldAutoSend),
+  guessLanguage: fallback(
+    picklist(["en", "fr"]),
+    defaultSettings.guessLanguage,
+  ),
+
+  atomView: fallback(
+    // FIXME don't know if this actually works
+    array(picklist(GUESS_OPTIONS.map(a => a.value))),
+    defaultSettings.atomView,
+  ),
+
+  hasChangedSettingsDuringGame: fallback(
+    boolean(),
+    defaultSettings.hasChangedSettingsDuringGame,
+  ),
+});
+
+const parsedStoredSettings = safeParse(
+  localStorageSettingsSchema,
+  JSON.parse(localStorage.getItem("settings") ?? "{}"),
+);
+
+const settings = parsedStoredSettings.success
+  ? parsedStoredSettings.output
+  : defaultSettings;
+
+// INFO Not the best solution ever, but it works really well
+export const settingsStore = proxy({
+  ...settings,
 
   toggleTimer: (force?: boolean) =>
     (settingsStore.shouldShowTimer = force ?? !settingsStore.shouldShowTimer),
@@ -25,7 +78,7 @@ export const settingsStore = proxy({
   toggleAutoSend: (force?: boolean) =>
     (settingsStore.shouldAutoSend = force ?? !settingsStore.shouldAutoSend),
 
-  atomView: GUESS_OPTIONS.map(a => a.value),
+  // Atom view related properties
   toggleAtomView: (value: GuessOptionValue, force?: boolean) => {
     if (force === true) {
       if (!settingsStore.atomView.includes(value))
@@ -46,6 +99,15 @@ export const settingsStore = proxy({
       settingsStore.atomView.push(value);
     }
   },
+
+  // Achievements-related properties
+  toggleChangedSettingsDuringGame: (force?: boolean) =>
+    (settingsStore.hasChangedSettingsDuringGame =
+      force ?? !settingsStore.hasChangedSettingsDuringGame),
+});
+
+subscribe(settingsStore, () => {
+  localStorage.setItem("settings", JSON.stringify(settingsStore));
 });
 
 export const useSettings = () => useSnapshot(settingsStore);
