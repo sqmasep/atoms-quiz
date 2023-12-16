74ea8abe770af0b0da0ae07ccb362c3c80949277
sqmasep <leblond.romain@orange.fr>
2023-12-16 02:41:37 +0100

    [FIX] More precise generic name in useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
-const useLocalStorage = <T>(key: string, initialValue: T) => {
-  const [value, setValue] = useState<T>(() => {
+const useLocalStorage = <TValue>(key: string, initialValue: TValue) => {
+  const [value, setValue] = useState<TValue>(() => {
     const storedValue = window.localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : initialValue;
   });
