0cb7c87b2b88b8740c35072c24071ec8771eb2a0
sqmasep <leblond.romain@orange.fr>
2023-12-17 01:36:20 +0100

    [FEAT] Added static colors depending on the groups/families..

---
+++ src/data/colors.ts
@@ -0,0 +1,64 @@
+import type { AtomType } from "~/lib/validation/atomSchema";
+
+type Color = `#${string}`;
+type BlockMode = Record<AtomType["block"], Color>;
+type PeriodMode = Record<AtomType["period"], Color>;
+type GroupMode = Record<AtomType["group"], Color>;
+type FamilyMode = Record<AtomType["family"]["name"], Color>;
+
+interface Modes {
+  block: BlockMode;
+  period: PeriodMode;
+  group: GroupMode;
+  family: FamilyMode;
+}
+
+export const COLORS = {
+  block: {
+    s: "#ff0000",
+    f: "#ff8000",
+    d: "#ffff00",
+    p: "#00ff00",
+  },
+  group: {
+    1: "#ff0000",
+    2: "#ff8000",
+    3: "#ffff00",
+    4: "#00ff00",
+    5: "#00ffff",
+    6: "#0000ff",
+    7: "#ff00ff",
+    8: "#ffffff",
+    9: "#000000",
+    10: "#808080",
+    11: "#800000",
+    12: "#808000",
+    13: "#008000",
+    14: "#008080",
+    15: "#000080",
+    16: "#800080",
+    17: "#c0c0c0",
+    18: "#808080",
+  },
+  period: {
+    1: "#ff0000",
+    2: "#ff8000",
+    3: "#ffff00",
+    4: "#00ff00",
+    5: "#00ffff",
+    6: "#0000ff",
+    7: "#ff00ff",
+  },
+  family: {
+    "Non-metal": "#5DFEFE",
+    "Post-transition metal": "#ffffff",
+    "Transition metal": "#959DEB",
+    Metalloid: "#AF6646",
+    "Noble gas": "#6FFF7D",
+    "Alkali metal": "#E0BF8D",
+    "Alkaline earth metal": "#5B8DA3",
+    Lanthanide: "#CBC05E",
+    Actinide: "#ff0000",
+    Halogen: "#999999",
+  },
+} as const satisfies Modes;
