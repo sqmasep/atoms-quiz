fea2a19deeb6ca508d0c205c64100b2333b8daa5
sqmasep <leblond.romain@orange.fr>
2023-12-29 02:39:54 +0100

    [FIX] useLocalStorage fix (?)

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,13 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const useLocalStorage = <TValue>(key: string, initialValue: TValue) => {
-  const [value, setValue] = useState<TValue>(() => {
-    const storedValue = window.localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initialValue;
-  });
+const useLocalStorage = <TValue>(key: string, defaultValue: TValue) => {
+  const isMounted = useRef(false);
+  const [value, setValue] = useState(defaultValue);
 
   useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(value));
+    const item = window.localStorage.getItem(key);
+    if (item !== null) {
+      setValue(JSON.parse(item) as TValue);
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [key]);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } else {
+      isMounted.current = true;
+    }
   }, [key, value]);
 
   return [value, setValue] as const;
