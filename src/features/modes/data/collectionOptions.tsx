236684b8d0848096903b0c3619aadf12910dc63b
sqmasep <leblond.romain@orange.fr>
2024-01-26 20:51:34 +0100

    [FEAT] Added collection options constants

--- src/features/modes/data/collectionOptions.tsx
+++ src/features/modes/data/collectionOptions.tsx
@@ -13,12 +13,76 @@ export const COLLECTION_OPTIONS = [
     icon: <EnterFullScreenIcon />,
   },
 
-  // FIXME a very bad idea, need to find a better way to do this
   {
-    value: "categories",
-    label: "Catégories",
+    value: "s-block",
+    label: "S-Block",
     icon: <CommitIcon />,
   },
+  {
+    value: "f-block",
+    label: "F-Block",
+    icon: <CommitIcon />,
+  },
+  {
+    value: "d-block",
+    label: "D-Block",
+    icon: <CommitIcon />,
+  },
+  {
+    value: "p-block",
+    label: "P-Block",
+    icon: <CommitIcon />,
+  },
+  // {
+  //   value: "alkali-metals",
+  //   label: "Alkali Metals",
+  //   icon: <CommitIcon />,
+  // },
+  // {
+  //   value: "alkaline-earth-metals",
+  //   label: "Alkaline Earth Metals",
+  //   icon: <CommitIcon />,
+  // },
+  // {
+  //   value: "transition-metals",
+  //   label: "Transition Metals",
+  //   icon: <CommitIcon />,
+  // },
+  // {
+  //   value: "post-transition-metals",
+  //   label: "Post Transition Metals",
+  //   icon: <CommitIcon />,
+  // },
+  // {
+  //   value: "metalloids",
+  //   label: "Metalloids",
+  //   icon: <CommitIcon />,
+  // },
+  // {
+  //   value: "nonmetals",
+  //   label: "Nonmetals",
+  //   icon: <CommitIcon />,
+  // },
+  // {
+  //   value: "halogens",
+  //   label: "Halogens",
+  //   icon: <CommitIcon />,
+  // },
+  // {
+  //   value: "noble-gases",
+  //   label: "Noble Gases",
+  //   icon: <CommitIcon />,
+  // },
+  // {
+  //   value: "lanthanides",
+  //   label: "Lanthanides",
+  //   icon: <CommitIcon />,
+  // },
+  // {
+  //   value: "actinides",
+  //   label: "Actinides",
+  //   icon: <CommitIcon />,
+  // },
 ] as const satisfies NonEmptyTuple<{
   value: string;
   label: string;
